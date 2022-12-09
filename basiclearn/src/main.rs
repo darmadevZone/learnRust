41c94b26248a8d250a8c5ab915f054c921d90696
darmadevZone <darmadevdone@gmail.com>
2022-12-09 23:52:32 +0900

    day2 branch

--- basiclearn/src/main.rs
+++ basiclearn/src/main.rs
@@ -1,5 +1,16 @@
+#![deny(clippy::all)]
+
 fn main() {
-    println!("Hello, world!");
-    println!("hello world");
-    println!("rust analyzer get extension plugin")
+    let name: &str = "foo";
+    let age: u8 = 20;
+    let num: i8 = 33;
+    let mut copy_name = &name;
+    let fruit = "banana";
+    let ref_fruit = &fruit;
+    let ref_ref_fruit = &&fruit;
+    println!("name:{},copy_name:{}", name, copy_name);
+    println!("age:{},num:{}", age, num);
+    test()
 }
+
+fn test() {}
